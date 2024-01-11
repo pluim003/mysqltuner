e6979d68adcee8f3acef4d438fb6438952afacf0
Dick Pluim <67138922+pluim003@users.noreply.github.com>
2024-01-11 08:20:28 +0100

    Sort order of the list of fragmented tables based on fragmentation ratio

--- MySQLTuner/Properties/AssemblyInfo.cs
+++ MySQLTuner/Properties/AssemblyInfo.cs
@@ -39,9 +39,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2024.1.9.26157")]
-[assembly: AssemblyFileVersion("2024.1.9.26157")]
+[assembly: AssemblyVersion("2024.1.11.13104")]
+[assembly: AssemblyFileVersion("2024.1.11.13104")]
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguage("en-NZ")]
 
-[assembly: AssemblyInformationalVersion("2024.1.9.26157")]
+[assembly: AssemblyInformationalVersion("2024.1.11.13104")]
